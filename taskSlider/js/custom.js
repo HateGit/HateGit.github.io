3f2fc2b82659dba830e7cee6c86bc366ee72eeab
HateGit <blackhawkdown12mm@mail.ru>
2018-05-23 16:03:59 +0400

    slider

---
+++ taskSlider/js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	loop:true,
+    // nav:true,
+    // autoplay: 2000,
+    responsive:{
+        0:{
+            items:1
+        },
+      	700:{
+      			items:3
+      	}
+    }
+  });
+});
