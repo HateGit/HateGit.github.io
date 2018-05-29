eb17b85c467ea69c0aaa9fd931a180f197bfb324
HateGit <blackhawkdown12mm@mail.ru>
2018-05-29 12:05:54 +0400

    re

--- landpage/js/main-accordion.js
+++ landpage/js/main-accordion.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
 
+// ACCORDION
+  $('.accordion-list__item').click(function() {
+    $(this).toggleClass('open').next('.accordion-content').slideToggle('.accordion-content');
+    $('.accordion-list__item').not(this).removeClass('open');
+  });
 
+
+//tabs sidebar
+	$('.tab-menu__chapter').click(function() {
+  $(this).toggleClass('open').next('.tab-submenu').slideToggle;
+  $('.tab-menu__chapter').not(this).removeClass('open')
+});
+
+//main FORM tabs
+
+  $('.form-tabs__nav-item').click(function() {
+  $(this).addClass('active');
+  $('.form-tabs__nav-item').not(this).removeClass('active');
+  $('.form-tabs__container').toggleClass('is-active');
+  // $('.form-tabs__container').not(this).removeClass('is-active');
+});
 });
